a614eb532f45c6597c5bf6d56f6e5959a45c53dd
geetads <45489768+geetads@users.noreply.github.com>
2019-10-17 13:36:06 -0400

    Create ContactsList.js

---
+++ ContactsList.js
@@ -0,0 +1,67 @@
+import React,{Component} from 'react'
+import ListContacts from {./ListContacts}
+
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
+
+
+ class App extends Component{
+render(){
+  return(
+    <div>
+
+      <ListContacts contacts={contacts} />
+      
+    </div>
+  )
+}
+
+
+ }
+ 
+ 
+ /* ListContacts*/
+ 
+ import React,{Component} from 'react'
+ 
+
+
+class ListContacts extends Component {
+    render(){
+        
+        return(
+            
+            <ol className = 'contact-list'>
+                {this.props.contacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.name}
+          </li>
+        ))}
+             </ol>
+        )
+    }
+
+}
+
+export default ListContacts
+
+
+ 
